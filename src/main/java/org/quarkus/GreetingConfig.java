e279ddedd42cd7dde3127f166516b50cf7eeb1ee
Muhammad Yusuf Alpian <llawliet1945@gmail.com>
2024-02-22 22:41:55 +0700

    initial commit

---
+++ src/main/java/org/quarkus/GreetingConfig.java
@@ -0,0 +1,12 @@
+package org.quarkus;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
